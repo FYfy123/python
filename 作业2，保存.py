d2f1ff8793ea985d1b0b51b0a424baa941687778
樊 <51935650+FYfy123@users.noreply.github.com>
2019-06-22 09:24:56 +0800

    Update 作业2，保存.py

--- 作业2，保存.py
+++ 作业2，保存.py
@@ -3,8 +3,8 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
 html = request.get('http://www.17k.com/chapter/2933095/36699279.html')
-with open('q.text'.'w')as f:
-   f.write(html.text)
+with open('q.text'.'w',encoding='utf8')as f:
+   f.write(html)
 data = urllib.parse.urlencode({'wd':'q'})
 print(data)
 url = 'http://www.baidu.com/s?' + data
